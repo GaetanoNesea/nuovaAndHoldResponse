19d73854e1f1c70d69bd7cbdd254f2101d0f30f9
Gaetano Sabino <g.sabino@nesea.it>
2022-01-18 08:59:26 +0100

    secondo commit

--- src/app/service/richiesta.service.ts
+++ src/app/service/richiesta.service.ts
@@ -2,11 +2,38 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {forkJoin, Observable} from 'rxjs';
 import {map} from 'rxjs/operators';
+import {isArray} from 'rxjs/internal-compatibility';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RichiestaService {
+
+  private static _nuovoVecchio<T, K extends keyof T, A>(obj: T, obj2: T): T {
+    let newobj = !!obj ? obj : obj2;
+
+    for (const objKey in newobj) {
+      if (obj2[objKey]) {
+        newobj = {
+          ...newobj, [objKey]: {
+            Vecchio: !!obj ? obj[objKey] : null,
+            Nuovo: obj2[objKey]
+          }
+        };
+      } else {
+        delete newobj[objKey];
+      }
+    }
+    return newobj;
+  }
+
+  private static _keyId<T>(obj: T): string {
+    for (const objKey in obj) {
+      if (objKey.indexOf('id', 0) > -1) {
+        return objKey;
+      }
+    }
+  }
 
   constructor(
     readonly http: HttpClient
@@ -34,33 +61,102 @@ export class RichiestaService {
     );
   }
 
-  public richiestaFor<A, B>(primoObj: string, secondoObj: string): Observable<{primo: A, secondo: B}> {
+
+  public richiestaFor<A, B>(primoObj: string, secondoObj: string): Observable<{ responseOriginale: A, responseNuova: B }> {
     return forkJoin(
       {
-        primo: this.richiesta(primoObj, 'dto', 'dati-completi-oe'),
-        secondo: this.richiesta(secondoObj, 'dto', 'dati-completi-oe')
+        responseOriginale: this.richiesta(primoObj, 'dto', 'dati-completi-oe'),
+        responseNuova: this.richiesta(secondoObj, 'dto', 'dati-completi-oe')
       }
     ).pipe(
-      map(({primo, secondo}: { primo: A, secondo: B }) => {
+      map(({responseOriginale, responseNuova}: { responseOriginale: A, responseNuova: B }) => {
         // const obj: { nuovo: any, vecchio: any };
 
-        for (const primoKey in primo) {
+        for (const keyResponseOriginale in responseOriginale) {
+          /**
+           * Prima cosa da fare, verificare che la stessa proprietà esista in entrambi gli elementi,
+           * altrimenti eliminiamo la stessa dal secondo array
+           */
           // @ts-ignore
-          if (primo[primoKey] && secondo[primoKey]) {
-            // return obj = {
-            //   nuovo: secondo[primoKey],
-            //   vecchio: primo[primoKey]
-            // };
-            primo = {...primo, [primoKey]: {
-                vecchio: primo[primoKey],
+          if (responseOriginale[keyResponseOriginale] && responseNuova[keyResponseOriginale]) {
+            /**
+             * Prima valutiamo se abbiamo un Array oppure un object
+             */
+            if (isArray(responseOriginale[keyResponseOriginale])) {
+              let key: string;
+              let newItemModifica = [];
+
+
+              /**
+               * Facendo una iterazione all'interno della lista modifico gli elementi della lista della responseOriginale in modo
+               * dhe dopo la possiamo sostituire alla responseOriginale.
+               */
                 // @ts-ignore
-                nuovo: secondo[primoKey]
-              }};
+              const arrayModificato = (responseOriginale[keyResponseOriginale]).map((item, index) => {
+
+                  if (!isArray(item)) {
+
+                    /** troviamo la key all'interno del item */
+                    key = RichiestaService._keyId(item);
+
+                    /** Cerchiamo nella seconda response se esiste un item con lo stesso id, significa ch'è stato modificato */
+                      // @ts-ignore
+                    const itemModificato = responseNuova[keyResponseOriginale].find((itemSecondo) => itemSecondo[key] === item[key]);
+
+                    /** All'inizio abbiamo creato una lista vuota, in questo caso verifichiamo che la lista è vuota, nel caso che lo fosse la
+                     * andiamo a riempire con il primo filtro dalla secondaResponse e successivamente elimineremo altri elementi
+                     */
+                    if (newItemModifica.length === 0) {
+                      // @ts-ignore
+                      newItemModifica = [...responseNuova[keyResponseOriginale].filter(itemSecondo => itemSecondo[key] !== item[key])];
+                    } else {
+                      newItemModifica = newItemModifica.filter(itemnew => itemnew[key] !== item[key]);
+                    }
+
+                    /** Nel caso che esista un elemento con lo stesso id nella lista primaResponse e secondaResponse
+                     * allora modifichiamo item della primaResponse con aggiunta di Vecchio e Nuovo
+                     */
+                    if (itemModificato) {
+                      item = RichiestaService._nuovoVecchio(item, itemModificato);
+                    } else {
+                      item = null;
+                    }
+                    /** Restituisce item modificato oppure a null */
+                    return item;
+                  }else{
+                    /** Nel caso che l'item è una lista */
+                    return item;
+                  }
+
+                });
+
+              /** Adesso prendiamo gli elementi che non sono presenti nella primaResponse e li modifichiamo,
+               * aggiungendo Vecchio e Nuovo
+               */
+              newItemModifica = newItemModifica.map(newItem => RichiestaService._nuovoVecchio(null, newItem));
+
+              /** Modifichiamo le responseOriginale e con la lista modificata */
+              responseOriginale = {...responseOriginale, [keyResponseOriginale]: [...arrayModificato.filter(itemModificato => !!itemModificato), ...newItemModifica]};
+            } else {
+              /**
+               * In questo caso abbiamo un object da valutare, inserendo un nuovo elemento o un vecchio elemento
+               */
+              responseOriginale = {
+                ...responseOriginale, [keyResponseOriginale]: {
+                  Vecchio: responseOriginale[keyResponseOriginale],
+                  // @ts-ignore
+                  Nuovo: responseNuova[keyResponseOriginale]
+                }
+              };
+            }
           } else {
-            // return {primo: {...primo}, secondo: {...secondo}};
+            /**
+             * Eliminiamo la proprietà non presente nel secondo elemento dal primo
+             */
+            delete responseOriginale[keyResponseOriginale];
           }
         }
-        return {primo, secondo};
+        return {responseOriginale, responseNuova};
       })
     );
   }
